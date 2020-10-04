728c52dbd530dc3178b204b1c508ff9def3279c3
repl.it user <replituser@example.com>
2020-10-04 03:50:42 +0000

    Finished How Far Away is...?

--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -4,6 +4,30 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
+			//Intializes the variables 
+			double startLat, startLong, finishLat, finishLong; 
+
+			//Asks the user for the value
+			Scanner in = new Scanner(System.in);
+
+			System.out.print("Enter the latitude of the starting location: ");
+			startLat = in.nextDouble(); 
+
+			System.out.print("Enter the longitude of the starting location: "); 
+			startLong = in.nextDouble(); 
+
+			System.out.print("Enter the latitude of the ending location: ");
+			finishLat = in.nextDouble(); 
+
+			System.out.print("Enter the longitude of the ending location: "); 
+			finishLong = in.nextDouble();
+
+
+			//Creates the startLocation and finishLocation object
+      GeoLocation startLocation = new GeoLocation(startLat, startLong); 
+			GeoLocation finishLocation = new GeoLocation(finishLat, finishLong); 
+
+			//Calculates and prints the distance
+			System.out.print("The distance is " + startLocation.distanceFrom(finishLocation) + " miles");
     }
 }
