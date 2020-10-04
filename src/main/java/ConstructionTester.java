ae731a3b5d656b80198d20319d6babbd97869f9a
Danae Dunlap <danae.736256@nv.ccsd.net>
2020-10-04 03:19:46 +0000

    Finished Construction cost

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,33 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+			
+      // intializes the variable 
+			double salesTax, total, grandTotal; 
+			int numBoards, numWindows; 
+
+			// ask the user for the value
+			Scanner in = new Scanner(System.in); 
+
+			System.out.println("Enter the sales tax rate: "); 
+			salesTax = in.nextDouble(); 
+
+			System.out.println("How many boards do you need? "); 
+			numBoards = in.nextInt(); 
+
+			System.out.println("How many windows do you need? ");
+			numWindows = in.nextInt(); 
+
+     // creates the construct object
+			Construction construct = new Construction(8.00, 11.00, salesTax); 
+
+			// calculates the grand total
+			total = construct.lumberCost(numBoards) + construct.windowCost(numWindows); 
+
+			grandTotal = construct.grandTotal(total);
+
+			System.out.println("Total: " + total);
+			System.out.println("Grand Total: " + grandTotal);  
+
     }
 }
